d88056abbfdedb6de48d07a327547db518a6b4e4
Tom Brandon <tom.j.v.brandon@gmail.com>
2017-12-12 18:00:00 +0000

    Reverted to DATABASE URL

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,7 +4,7 @@ require 'dm-postgres-adapter'
 
 class Link
   include DataMapper::Resource
-  DataMapper.setup(:default, ENV['HEROKU_POSTGRESQL_BROWN_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+  DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 
   property :id, Serial
   property :title, String
